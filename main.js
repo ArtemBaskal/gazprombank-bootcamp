90e6ad6ce1b106895e4c7c4d5ba6dcd7014cadc6
ArtemBaskal <asbaskal@edu.hse.ru>
2019-01-17 17:20:04 +0300

    Changed the JSON file

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("Hello World!")
+
+obj = {
+    number: 123.456,
+    string: "Hello GPB",
+    boolean: true,
+    object: {
+        name: "Vasya",
+        arr: [0, 1, 2]
+    },
+    null: null,
+}
+
+console.log(obj)
+console.log(JSON.stringify(obj))
