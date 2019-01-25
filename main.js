2333727094c8e5f473d4f7a97d9e5252f553983e
ArtemBaskal <asbaskal@edu.hse.ru>
2019-01-25 23:55:14 +0300

    three tasks have been done

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 function printWindowValues() {
-  const arr = Object.getOwnPropertyNames(window);
-  for (const key of arr) {
+  const arrOfProps = Object.getOwnPropertyNames(window);
+  for (const key of arrOfProps) {
     console.log(key);
   }
-};
+}
 
 printWindowValues();
+
+function expten(number) {
+  let result = 1;
+  for (let i = 1; i <= 10; i++) {
+    result *= number;
+  }
+  console.log(result);
+}
+
+function getFuncRes(func, value) {
+  return func(value);
+}
+
+getFuncRes(console.log, "Hello World!");
