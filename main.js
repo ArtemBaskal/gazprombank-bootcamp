24270365e1d42bd21ef5a643329cdf434c806978
ArtemBaskal <asbaskal@edu.hse.ru>
2019-01-26 19:52:12 +0300

    Mostly refactoring

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-function printWindowValues() {
-  const arrOfProps = Object.getOwnPropertyNames(window);
-  for (const key of arrOfProps) {
-    console.log(key);
-  }
-}
+//task 1
+
+let printWindowValues = () =>
+  Object.getOwnPropertyNames(window).forEach(prop => console.log(window[prop]));
 
 printWindowValues();
 
-function expten(number) {
-  let result = 1;
-  for (let i = 1; i <= 10; i++) {
-    result *= number;
-  }
-  console.log(result);
-}
+//task 2
 
-function getFuncRes(func, value) {
+let expten = function(num) {
+  let res = 1;
+  for (let i = 1; i <= 10; i++) {
+    res *= num;
+  }
+  console.log(res);
+};
+
+expten(2); //example
+
+//task 3
+
+let getFuncRes = function(func, value) {
   return func(value);
-}
+};
 
 getFuncRes(console.log, "Hello World!");
