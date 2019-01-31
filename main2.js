0b61a4eccc3fc580b63950574dc0c8dad16ef98e
ArtemBaskal <asbaskal@edu.hse.ru>
2019-01-31 22:05:54 +0300

    task 2 is completed

--- main2.js
+++ main2.js
@@ -1,1 +1,47 @@
+(function() {
+  function recursion() {
+    const child = document.querySelector(".deepChild");
+    //child.style.background = "lightBlue";
 
+    function drawRed(el) {
+      el.style.border = "3px solid red";
+      el.style.margin = "12px";
+      el.style.padding = "12px";
+    }
+    function drawGreen(el) {
+      el.style.border = "3px solid green";
+      el.style.margin = "12px";
+      el.style.padding = "12px";
+    }
+    function catchParent(el) {
+      drawRed(el);
+      const parent = el.parentNode;
+      const atr = parent.getAttribute("class");
+      if (atr != "sibling first") {
+        setTimeout(() => {
+          drawRed(parent);
+          catchParent(parent);
+        }, 300);
+    } else {
+            const sibling = parent.nextElementSibling;
+            catchChild(sibling);          
+         };
+      }
+    catchParent(child);
+
+    function catchChild(el) {
+      drawGreen(el);
+      const child = el.firstElementChild;
+      const atr = child.getAttribute("class");
+      if (atr != "deepChild") {
+        setTimeout(() => {
+          drawGreen(child);
+          catchChild(child);
+        }, 300);
+      }
+    }
+    catchChild(sibling);
+  }
+
+  recursion();
+})();
