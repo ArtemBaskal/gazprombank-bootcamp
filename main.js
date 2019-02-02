3c18c742d504b0e466fda19adc14f827d5e532da
ArtemBaskal <asbaskal@edu.hse.ru>
2019-02-02 22:53:10 +0300

    It works!

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
+const dragElement = document.querySelector(".dragElement");
 
+dragElement.addEventListener("mousedown", e => {
+  let coords = getCoords(dragElement);
+  let shiftX = e.pageX - coords.left;
+  let shiftY = e.pageY - coords.top;
+
+  dragElement.style.position = "absolute";
+
+  moveAt(e);
+
+  function moveAt(e) {
+    dragElement.style.left = e.pageX - shiftX + "px";
+    dragElement.style.top = e.pageY - shiftY + "px";
+  }
+
+  const eventMoveAt = function(e) {
+    return moveAt(e);
+  };
+
+  document.addEventListener("mousemove", eventMoveAt);
+
+  dragElement.addEventListener("mouseup", () => {
+    document.removeEventListener("mousemove", eventMoveAt);
+    dragElement.removeEventListener("mouseup", eventMoveAt);
+  });
+
+
+});
+
+dragElement.addEventListener("dragstart", () => false);
+
+function getCoords(elem) {
+  const box = elem.getBoundingClientRect();
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset
+  };
+}
