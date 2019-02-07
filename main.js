34a48180fe50fd5af764e315367b66976a0d64d3
ArtemBaskal <asbaskal@edu.hse.ru>
2019-02-07 19:48:05 +0300

    final

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
+//task 1
+(function() {
+  function getSevenCol(arr) {
+    let i = 0;
+    arr
+      .toString()
+      .split("")
+      .forEach(el => {
+        if (+el === 7) i++;
+      });
+    return i;
+  }
+  console.log(getSevenCol([1, 7, 4, 77, 73])); //example
+})();
 //task 2
-const arr = ["a", "b", "c", "d", "e", "f"];
-const length = arr.length;
-let  brr = [];
-for (let i=0; i < length/2; i++){
-brr.push(arr.shift(), arr.pop())
-}
-console.log(brr.join(""))
+(function() {
+  function arrToStr(arr) {
+    const halfLength = arr.length / 2;
+    let brr = [];
+    for (let i = 0; i < halfLength; i++) {
+      brr.push(arr.shift(), arr.pop());
+    }
+    return brr.join("");
+  }
+
+  console.log(arrToStr(["a", "b", "c", "d", "e", "f"])); //example
+})();
