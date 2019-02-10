eaf36024ed75a286783053d42f2dcfa1ee17f01e
ArtemBaskal <asbaskal@edu.hse.ru>
2019-02-10 16:23:30 +0300

    Working commit

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+function throughOnce(fn) {
+  let result;
+  let isEven = true;
 
+  return function() {
+    if (isEven) {
+      result = fn.apply(this, arguments);
+    }
+    isEven = !isEven;
+    return result;
+  };
+}
+
+const logThroughOnce = throughOnce(function(text) {
+  console.log(text);
+});
+
+logThroughOnce("Hello World! 1"); // "Hello World! 1"
+logThroughOnce("Hello World! 2"); //
+logThroughOnce("Hello World! 3"); // "Hello World! 3"
+logThroughOnce("Hello World! 4"); //
