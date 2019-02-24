970aacf69ef358a80105f8b1e1b99b9405e2306b
ArtemBaskal <asbaskal@edu.hse.ru>
2019-02-25 01:12:05 +0300

    Working version

--- task2/main.js
+++ task2/main.js
@@ -1,1 +1,23 @@
+let data = 1;
+let time = new Date().getTime();
+let isFirstCall = true;
+let arr = [];
 
+function getData() {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(data++), 1000);
+  });
+}
+
+setInterval(() => {
+  let currentTime = new Date().getTime();
+  if (currentTime - time >= 2000 || isFirstCall) {
+    getData().then(resolve => {
+      arr.push(resolve);
+      time = currentTime;
+      isFirstCall = false;
+    });
+  } else {
+    console.log("Информация закэширована: " + arr[arr.length - 1]);
+  }
+}, 500);
